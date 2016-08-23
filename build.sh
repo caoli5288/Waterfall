245ec25f5e0b69c008b939907f90ebc1f1be74ca
caoli5288 <caoli5288@gmail.com>
2016-08-24 05:42:52 +0800

    Do not prevent bungee-bungee connection

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-git submodule update --init && ./applyPatches.sh && pushd Waterfall-Proxy && mvn clean package && popd
+git submodule update --init && ./applyPatches.sh && pushd Waterfall-Proxy && mvn clean package -Dmaven.test.skip=true && popd
 
